4bf70e99f790c566cf4d5e58872a0c8e5ec1906a
aerodes <93395783+aerodes@users.noreply.github.com>
2024-10-01 14:52:45 +0300

    Update index.tsx

--- source/index.tsx
+++ source/index.tsx
@@ -3,7 +3,7 @@ import Template from "./pages";
 import "./style.scss";
 
 document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.createRoot(document.getElementById("root") as Element).render(
+  ReactDOM.createRoot(document.getElementById("root")).render(
     <Template />
   );
 });
