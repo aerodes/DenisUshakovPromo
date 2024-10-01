d55469f2f5ac216603e58c7b44f8eb64777475b0
aerodes <93395783+aerodes@users.noreply.github.com>
2024-10-01 14:39:19 +0000

    fix

--- source/index.tsx
+++ source/index.tsx
@@ -1,9 +1,9 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM, { Container } from "react-dom/client";
 import Template from "./pages";
 import "./style.scss";
 
 document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.createRoot(document.getElementById("root") as Element).render(
+  ReactDOM.createRoot(document.getElementById("root") as Container).render(
     <Template />
   );
 });
