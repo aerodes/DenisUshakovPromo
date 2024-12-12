2acd5f2894f8cce2f673f0385955275f50fdd70d
aerodes <93395783+aerodes@users.noreply.github.com>
2024-12-12 07:57:54 +0000

    fix test btn

--- source/pages/index.tsx
+++ source/pages/index.tsx
@@ -14,18 +14,34 @@ const Template = () => {
     );
   }, 1000);
 
-  const view = () => {
+  const initTG = () => {
     // @ts-ignore
-    const tg = window.Telegram.WebApp;
-
+    const tg = window?.Telegram?.WebApp;
     return tg
+  }
+
+  const tg = initTG();
+
+  const click = () => {
+    try {
+      tg.openTelegramLink(`tg://user?id=${1827844160}`);
+    } catch {
+      try {
+        tg.openLink(`tg://user?id=${1827844160}`);
+      } catch {
+        window.open(`tg://user?id=${1827844160}`);
+      }
+    }
   }
 
   return (
     // <div className={styles["div"]} style={{ color: color, background: "#000" }}>
     //   Кристина лучшая девушка!!!
     // </div>
-    <div style={{ background: "#D4EFF7", height: "100vh", width: "100vw" }}>{view().initData}</div>
+    <div style={{ background: "#D4EFF7", height: "100vh", width: "100vw" }}>
+      <div>{tg.initData}</div>
+      <button onClick={() => click()}>ссылка на Женю</button>
+    </div>
   );
 };
 
