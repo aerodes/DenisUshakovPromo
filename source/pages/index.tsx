4c9b12e7c690a5191b3034fbd5cdced883a3462d
aerodes <93395783+aerodes@users.noreply.github.com>
2024-12-12 15:04:56 +0000

    test orient

--- source/pages/index.tsx
+++ source/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as styles from "./index.module.scss";
 
 const Template = () => {
@@ -20,6 +20,24 @@ const Template = () => {
     return tg
   }
 
+  const setBGColor = (webApp, color) => {
+    webApp.setBottomBarColor(color);
+  }
+
+  useEffect(() => {
+    setBGColor(initTG(), "#ffffff");
+  }, [])
+
+  useEffect(() => {
+    const div = document.getElementById("orient");
+    initTG().DeviceOrientation.start(20, false, (orientation) => {
+      if (div) {
+        div.innerText = `alpha: ${orientation.alpha}, beta: ${orientation.beta}, gamma: ${orientation.gamma}`;
+      } else console.warn("div для вывода ориентации не найден!!!");
+
+    })
+  })
+
   return (
     // <div className={styles["div"]} style={{ color: color, background: "#000" }}>
     //   Кристина лучшая девушка!!!
@@ -28,7 +46,8 @@ const Template = () => {
       <div style={{ marginTop: "40px", width: "100%" }}>{`${initTG()?.initData}`}</div>
       <div style={{ marginTop: "40px" }}>{`${initTG()?.initDataUnsafe?.user?.username}`}</div>
       <div style={{ marginTop: "40px" }}>Загрузилось!!!</div>
-      <button onClick={() => initTG().downloadFile("https://i.pinimg.com/736x/82/2a/1d/822a1d20af7ef527f3dbb2636b1568e7.jpg")}>загрузить изображение</button>
+      <div style={{ marginTop: "40px" }} id="orient"></div>
+      {/* <button onClick={() => initTG().downloadFile("https://i.pinimg.com/736x/82/2a/1d/822a1d20af7ef527f3dbb2636b1568e7.jpg")}>загрузить изображение</button> */}
     </div>
   );
 };
