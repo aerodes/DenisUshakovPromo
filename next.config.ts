e429a4e921aecc3acf2d4d7adf17600ed34db6de
aerodes <93395783+aerodes@users.noreply.github.com>
2025-02-14 09:10:09 +0000

    test

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",  // <=== enables static exports
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
   reactStrictMode: true,
-  basePath: "/DenisUshakovPromo",
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/your-repository-name/' : '',
+  basePath: isProd ? '/your-repository-name' : '',
+  output: 'export'
 };
 
 export default nextConfig;
