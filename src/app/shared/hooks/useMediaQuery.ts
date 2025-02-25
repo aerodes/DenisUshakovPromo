1ae0b7a73aea7be2578797f099863109450c5d70
raccoon <aerodes.us@gmail.com>
2025-02-25 21:24:06 +0300

    add adaptive

--- src/app/shared/hooks/useMediaQuery.ts
+++ src/app/shared/hooks/useMediaQuery.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const useMediaQuery = (query: any) => {
   const [matches, setMatches] = useState(false);
 
