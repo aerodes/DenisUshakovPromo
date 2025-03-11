493332229a45c79b42031f403f817191e175dbb7
Ушаков Денис В <d.ushakov@realize.dev>
2025-03-11 17:40:21 +0300

    добавил 2 раздел

--- src/pages/Wedding/components/Greeting/index.tsx
+++ src/pages/Wedding/components/Greeting/index.tsx
@@ -1,9 +1,25 @@
 "use client";
 
+import ImageCustom from "@/src/shared/components/ImageCustom";
 import styles from "./index.module.scss";
 
 const MainCover = (): React.ReactElement => {
-    return <div className={styles.main}>Дорогие гости!</div>;
+    return (
+        <div className={styles.main}>
+            <div className={styles.main_title}>Дорогие гости!</div>
+            <div className={styles.main_title_sub}>
+                Приглашаем вас разделить с нами радость особенного дня - нашей свадьбы!
+            </div>
+            <div className={styles.main_photo_container}>
+                <div className={styles.main_photo_container_1}>
+                    <ImageCustom src="./shared/images/greeting_1.png" />
+                </div>
+                <div className={styles.main_photo_container_2}>
+                    <ImageCustom src="./shared/images/greeting_2.png" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default MainCover;
---
+++ src/shared/components/ImageCustom/index.tsx
@@ -0,0 +1,43 @@
+import {
+    ImageLoader,
+    OnLoadingComplete,
+    PlaceholderValue,
+    StaticImport,
+} from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+const ImageCustom = ({
+    ...args
+}: {
+    src: string | StaticImport;
+    alt?: string;
+    width?: number | `${number}`;
+    height?: number | `${number}`;
+    fill?: boolean;
+    loader?: ImageLoader;
+    quality?: number | `${number}`;
+    priority?: boolean;
+    loading?: "eager" | "lazy" | undefined;
+    placeholder?: PlaceholderValue;
+    blurDataURL?: string;
+    unoptimized?: boolean;
+    overrideSrc?: string;
+    onLoadingComplete?: OnLoadingComplete;
+    layout?: string;
+    objectFit?: string;
+    objectPosition?: string;
+    lazyBoundary?: string;
+    lazyRoot?: string;
+}): React.ReactElement => {
+    return (
+        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+            <Image
+                {...args}
+                src={args.src}
+                alt={args.alt ? args.alt : " "}
+                layout={args.layout ? args.layout : "fill"}
+                objectFit={args.objectFit ? args.objectFit : "contain"}
+            />
+        </div>
+    );
+};
+export default ImageCustom;
