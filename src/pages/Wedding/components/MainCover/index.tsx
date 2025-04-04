fab98ce98e2dd166422444188ecf2f8424293883
Ушаков Денис В <d.ushakov@realize.dev>
2025-04-04 17:32:38 +0300

    add load animation

--- src/pages/Wedding/components/MainCover/index.tsx
+++ src/pages/Wedding/components/MainCover/index.tsx
@@ -6,6 +6,7 @@ import Logo from "@/public/shared/icons/main_logo.svg";
 const MainCover = (): React.ReactElement => {
     return (
         <div className={styles.main}>
+            <div className={styles.main_cover} />
             <div className={styles.main_container}>
                 <div className={styles.main_container_D}>D</div>
                 <div className={styles.main_container_K}>K</div>
